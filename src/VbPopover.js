90e10b71af780a1a946ffc2d987c9b1b7cef6ab6
Paul Tuckey <paul@tuckey.org>
2021-05-20 10:30:56 +1200

    progress

--- src/VbPopover.js
+++ src/VbPopover.js
@@ -4,6 +4,33 @@ export default {
 
     createIsHandler(Popover, el, binding) {
         //console.log('popover createIsHandler')
+
+        let showEventHandler = () => {
+            let evt = document.createEvent('HTMLEvents')
+            evt.initEvent('vb-show-bs-popover', true, true)
+            el.dispatchEvent(evt)
+        }
+        let shownEventHandler = () => {
+            let evt = document.createEvent('HTMLEvents')
+            evt.initEvent('vb-shown-bs-popover', true, true)
+            el.dispatchEvent(evt)
+        }
+        let hideEventHandler = () => {
+            let evt = document.createEvent('HTMLEvents')
+            evt.initEvent('vb-hide-bs-popover', true, true)
+            el.dispatchEvent(evt)
+        }
+        let hiddenEventHandler = () => {
+            let evt = document.createEvent('HTMLEvents')
+            evt.initEvent('vb-hidden-bs-popover', true, true)
+            el.dispatchEvent(evt)
+        }
+        let insertedEventHandler = () => {
+            let evt = document.createEvent('HTMLEvents')
+            evt.initEvent('vb-inserted-bs-popover', true, true)
+            el.dispatchEvent(evt)
+        }
+
         return {
             beforeMount() {
                 //console.log('popover beforeMount', el)
@@ -11,6 +38,11 @@ export default {
                 let ins = Popover.getInstance(el)
                 if (!ins) ins = new Popover(el, binding.value)
                 el.$vb.popover = ins
+                el.addEventListener('show.bs.popover', showEventHandler)
+                el.addEventListener('shown.bs.popover', shownEventHandler)
+                el.addEventListener('hide.bs.popover', hideEventHandler)
+                el.addEventListener('hidden.bs.popover', hiddenEventHandler)
+                el.addEventListener('inserted.bs.popover', insertedEventHandler)
             },
             updated() {
                 let ins = Popover.getInstance(el)
@@ -20,6 +52,13 @@ export default {
                 let ins = Popover.getInstance(el)
                 if (ins) ins.dispose()
                 el.$vb.popover = undefined
+            },
+            unmounted() {
+                el.removeEventListener('show.bs.popover', showEventHandler)
+                el.removeEventListener('shown.bs.popover', shownEventHandler)
+                el.removeEventListener('hide.bs.popover', hideEventHandler)
+                el.removeEventListener('hidden.bs.popover', hiddenEventHandler)
+                el.removeEventListener('inserted.bs.popover', insertedEventHandler)
             }
         }
     },
