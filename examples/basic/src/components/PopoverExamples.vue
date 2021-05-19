da98da034e52c01d7ebe67d7b5d74b7f7b71c771
Paul Tuckey <paul@tuckey.org>
2021-05-20 10:23:55 +1200

    progress

--- examples/basic/src/components/PopoverExamples.vue
+++ examples/basic/src/components/PopoverExamples.vue
@@ -4,8 +4,26 @@
       <div class="mb-4">
           <h2>Popover</h2>
 
-          <button v-vb-is:popover type="button" class="btn btn-lg btn-danger" data-bs-toggle="popover"
+          <button v-vb-is:popover type="button" class="btn btn-secondary me-2"
+                  data-bs-toggle="popover"
                   title="Popover title" data-bs-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
+
+          <button v-vb-is:popover type="button" class="btn btn-secondary me-2"
+                  data-bs-toggle="popover"
+                  data-bs-trigger="hover"
+                  title="Popover title" data-bs-content="And here's some amazing content. It's very engaging. Right?">Hover to toggle popover</button>
+
+          <div class="d-none">
+              <div ref="popoverExample1">
+                  Hello World
+                  <button type="button" class="btn btn-danger">Button</button>
+              </div>
+          </div>
+
+          <button
+            v-vb-is:popover="{content: () => $refs.popoverExample1}" type="button"
+            data-bs-trigger="hover focus" data-bs-html="true"
+            class="btn btn-secondary me-2">Click to toggle $ref popover</button>
 
       </div>
 
--- src/VbPopover.js
+++ src/VbPopover.js
@@ -7,14 +7,12 @@ export default {
         return {
             beforeMount() {
                 //console.log('popover beforeMount', el)
-                if (el.classList && !el.classList.contains('popover')) el.classList.add('popover')
                 if (!el.$vb) el.$vb = {};
                 let ins = Popover.getInstance(el)
                 if (!ins) ins = new Popover(el, binding.value)
                 el.$vb.popover = ins
             },
             updated() {
-                if (el.classList && !el.classList.contains('popover')) el.classList.add('popover')
                 let ins = Popover.getInstance(el)
                 if (ins) ins.update()
             },
@@ -25,36 +23,6 @@ export default {
             }
         }
     },
-
-    createToggleHandler(Popover, el, binding) {
-        //console.log('createToggleHandler', el, binding)
-        let clickHandler = async function (e) {
-            e.preventDefault()
-            e.stopPropagation()
-            //console.log('popover toggle click', el, binding.value, el.dataset.bsTarget)
-            let targetEl = null
-            if (binding.value) {
-                let refObj = binding.instance.$refs[binding.value]
-                targetEl = refObj && refObj.$el ? refObj.$el : refObj
-            } else if (el.dataset.bsTarget) {
-                targetEl = document.querySelector(el.dataset.bsTarget)
-            }
-            if (targetEl) {
-                let ins = Popover.getInstance(targetEl)
-                if (ins) ins.toggle(el)
-            }
-        }
-        return {
-            beforeMount() {
-                //console.log('popover toggle beforemount')
-                el.addEventListener('click', clickHandler);
-            },
-            beforeUnmount() {
-                el.removeEventListener('click', clickHandler);
-            }
-        }
-    },
-
 
 }
 
